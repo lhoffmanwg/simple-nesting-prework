1456d5d0307ae10ae9699bddf75c169ebcd6caa7
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-07 03:46:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,7 +89,8 @@ def adding_matz
      }
               
    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP, C"]}
-   #programmer_hash[:yukihiro_matsumoto] = {:languages => ["LISP, C"]}   
+   #programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby
+   #programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP, C"]  
    
    programmer_hash
 end
@@ -113,6 +114,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay] = {:known_for => alans_new_info}
+     
+     programmer_hash
      
      
 end
